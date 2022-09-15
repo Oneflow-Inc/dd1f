795c959ec888c78b220f492ab7538962ccb2453a
jackalcooper <jackalcooper@gmail.com>
2022-09-15 14:57:35 +0800

    add

--- oneflow/start-container.sh
+++ oneflow/start-container.sh
@@ -3,7 +3,6 @@ docker run --rm -it \
     -v $(echo ~)/disco-diffusion/images_out:/workspace/code/images_out \
     -v $(echo ~)/disco-diffusion/init_images:/workspace/code/init_images \
     --gpus=all \
-    --name dd-${USER} \
-    --name="disco-diffusion" --ipc=host \
+    --name="dd-${USER}" --ipc=host \
     --user $(id -u):$(id -g) \
 oneflowinc/dd1f:latest python disco-diffusion/disco.py
